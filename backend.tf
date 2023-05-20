a8cecf463b2bd48cc3a54b071c2a7932ec07fdb4
Dmitry Pitel <dpitel@gmail.com>
2023-05-19 21:13:22 -0400

    moved backend file

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name     = "tfstates"
-        storage_account_name    = "dimastore"
-        container_name          = "tfstatesdevops"
-        key                     = "tfstatesdevops.tfstate"
-    }
-}
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
         resource_group_name     = "tfstates"
         storage_account_name    = "dimastore"
         container_name          = "tfstatesdevops"
-        key                     = "terraform.tfstatesdevops"
+        key                     = "tfstatesdevops.tfstate"
     }
 }
