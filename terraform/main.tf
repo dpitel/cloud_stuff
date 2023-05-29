9d509a2da2f7c94e7476abc061b16a093b2cd363
Dmitry Pitel <dpitel@gmail.com>
2023-05-29 00:52:16 -0400

    adding new terraform block for storage blob and website stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,63 @@ provider "azurerm" {
 # Resources
 #
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "resource_group" {
   name     = "cloud_newest"
   location = "eastus"
+}
+
+#
+# Create Storage Account
+#
+resource "azurerm_storage_account" "storage_account" {
+  name                    = "cloud_newest_storage"
+  resource_group_name     = azurerm_resource_group.resource_group
+
+  location                = "eastus"
+  account_tier            = "Standard"
+  account_replication_type= "LRS"
+  account_kind            = "StorageV2"
+
+  static-website {
+    index_document = "index.html"
+  } 
+}
+
+#
+# Add index.html to blob storage
+#
+resource "azurerm_storage_blob" "index_html" {
+  name                    = "index.html" 
+  storage_account_name    = azurerm_storage_account.storage_account.name
+  storage_container_name  = "$web"
+  type                    = "Block"
+  content_type            = "test/html"
+  source_content          = file("$(path.module)/website/index.html")   
+}
+
+resource "azurerm_storage_blob" "my_pic" {
+  name                    = "my_pic.jpg"
+  storage_account_name    = azurerm_storage_account.storage_account.name  
+  storage_container_name  = "$web"
+  type                    = "Block"
+  content_type            = "image/jpg"
+  source_content          = file("$(path.module)/website/my_pic.jpg") 
+}
+
+resource "azurerm_storage_blob" "pex_2" {
+  name                    = "pex_2.jpg"
+  storage_account_name    = azurerm_storage_account.storage_account.name
+  storage_container_name  = "$web"
+  type                    = "Block"
+  content_type            = "image/jpg"
+  source_content          = file("$(path.module)/website/pex_2.jpg")     
+}
+
+resource "azurerm_storage_blob" "newest" {
+  name                    = "newest.css"
+  storage_account_name    = azurerm_storage_account.storage_account.name
+  storage_container_name  = "$web"
+  type                    = "Block"
+  content_type            = "text/css"
+  source_content          = file("$(path.module)/website/newest.css")
 }
